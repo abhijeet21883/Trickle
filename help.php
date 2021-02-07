3857909317e201586f6aa2123ee7470aa1bc618a
Abhijeet Kumar <53680728+abhijeet21883@users.noreply.github.com>
2021-02-07 19:16:05 +0530

    Update help.php

--- help.php
+++ help.php
@@ -4,14 +4,17 @@
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','tickle');
-	if($conn->connect_error){
+	if($conn->connect_error)
+	{
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
-	} else {
+	} 
+         else 
+        {
 		$stmt = $conn->prepare("insert into registration(username, password) values(?, ?)");
-		$stmt->bind_param( "ss",$username, $password);
+		$stmt->bind_param( "ss",$username, $password);//binding the parameters
 		$execval = $stmt->execute();
-		echo " Registration successfull";
+		echo " Registration successful";
 		$stmt->close();
 		$conn->close();
 	}
